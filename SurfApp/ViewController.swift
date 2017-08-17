96649e004d6013f32dfcf9ab921e161a4406e6b7
Pole Star <polestar@Stephanes-MacBook-Pro.local>
2017-08-17 10:43:02 -0700

    Initial Commit

---
+++ SurfApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfApp
+//
+//  Created by Pole Star on 8/17/17.
+//  Copyright © 2017 Pole Star USA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
